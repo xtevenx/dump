d83d276ce443490dfa3bfc0529212db1c65d4ec9
Steven Xia <stevenxia@protonmail.com>
2024-07-14 14:05:22 -0700

    yay all gooder

--- two_distinct_eigenvalues.py
+++ two_distinct_eigenvalues.py
@@ -1,77 +1,25 @@
-from os import makedirs
-
 from sage.all import *
 
-from utils import enumerate_graphs, spectral_radius
+from utils import *
 
+if __name__ == '__main__':
 
-def is_symmetric(p):
-    c = p.coefficients(sparse=False)
+    from pathlib import Path
 
-    i = 0
-    while c[i] == 0:
-        i = i + 1
+    data = []
 
-    for j in range(i + 1, len(c), 2):
-        if c[j] != 0:
-            return False
-    return True
-
-
-def save(location: str, order: int, oriented_only: bool = False) -> list:
-    info = []
-
-    for graph in enumerate_graphs(order):
-        G = DiGraph(graph)
+    for G in digraphs(5, augment='vertices'):
 
         if not G.is_connected():
             continue
-
-        if oriented_only:
-            A = G.adjacency_matrix()
-            if A.elementwise_product(A.transpose()) != 0:
-                continue
 
         M = G.adjacency_matrix() * E(6)
         M = M + conjugate(M.transpose())
         P = SR(M.charpoly())
 
-        solutions = solve(SR(P), var('x'), multiplicities=True)
+        solutions = solve(P, var('x'))
 
-        if len(solutions[0]) == 2:
-            id = len(info) + 1
-            fname = f'{location}/graph{id:03}.png'
-            print(f'Saving image {fname}')
-            G.plot(layout='spring', iterations=9001).save(fname)
+        if len(solutions) == 2:
+            data.append(G)
 
-            solutions = [(s.right_hand_side(), m) for s, m in zip(*solutions)]
-            solutions.sort(key=lambda t: t[0])
-
-            info.append({
-                'fname': fname,
-                'id': id,
-                'charpoly': latex(P),
-                'r': spectral_radius(P),
-                'spec': latex(solutions)
-            })
-
-    return info
-
-
-if __name__ == '__main__':
-    # jinja2 docs: https://jinja.palletsprojects.com/en/3.0.x/templates/
-
-    import jinja2
-
-    from pathlib import Path
-
-    FNAME = Path(__file__).stem
-
-    makedirs(FNAME, exist_ok=True)
-
-    data = save(FNAME, order=5)
-
-    with open(f'template.html') as fp:
-        html = jinja2.Template(fp.read()).render(list=data)
-        with open(f'{FNAME}.html', 'w') as fp:
-            fp.write(html)
+    save(Path(__file__).stem, data)
